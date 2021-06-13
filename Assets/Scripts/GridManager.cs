0919f8d3fc9e93c7a86b6f1b86c7f8bda744082a
Keiotic <54135223+Keiotic@users.noreply.github.com>
2021-06-13 17:19:18 +0200

    Add Grid Manager

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    public float gridSquareSize = 13.5f;
+    public Vector2 gridSize = new Vector2(9, 15);
+    private List<GameObject> gridReps = new List<GameObject>();
+    public GameObject gridRep;
+    void Start()
+    {
+        for (int j = 0; j < gridSize.y; j++)
+        {
+            for (int i = 0; i < gridSize.x; i++)
+            {
+                Vector2 gridPos = new Vector2((i - (gridSize.x-1)/2) * gridSquareSize / 2, (j - (gridSize.y - 1) / 2) * gridSquareSize/2);
+                GameObject g = Instantiate(gridRep, gridPos, Quaternion.identity);
+                gridReps.Add(g);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
